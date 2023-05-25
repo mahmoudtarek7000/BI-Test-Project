4b8d54aba5bb4f41637233402fd6d610643ee082
Basel ElSawy <basel.elsawy@bi-technologies.net>
2023-05-25 16:39:26 +0300

    fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,18 @@
-import { AfterViewChecked, AfterViewInit, ChangeDetectorRef, Component, OnInit, ViewChild } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { Validators } from '@angular/forms';
 import { IColumns } from 'bi-interfaces/lib/interfaces/IColumns.interface';
 import { DataTypes } from './enums/DataType';
-import { IGrid } from 'bi-interfaces/lib/interfaces/IGrid';
 import { DataService } from "./service/data.service";
+import { IGrid } from 'bi-interfaces/lib/interfaces/IGrid';
 
 @Component({
 	selector: 'app-root',
 	templateUrl: './app.component.html',
 	styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit, AfterViewInit {
+export class AppComponent implements OnInit {
 	title = 'BI-Project';
+	@ViewChild('Header') BIGrid!: IGrid;
 	columns: IColumns[] = [
 		{ DisplayName: "CategoryId", Name: "CategoryId", DataType: DataTypes.Text, controlType: "text", Validators: Validators.required, IsEditable: true, IsFilterable: true, DefaultValue: null, viewCellStyle: "", IsVisible: true },
 		{ DisplayName: "Description", Name: "Description", DataType: DataTypes.Text, controlType: 'text', Validators: Validators.required, IsEditable: true, IsFilterable: true, DefaultValue: null, viewCellStyle: "", IsVisible: true },
@@ -24,39 +25,11 @@ export class AppComponent implements OnInit, AfterViewInit {
 		{ DisplayName: "DefaultCreditLimit", Name: "DefaultCreditLimit", DataType: DataTypes.NUMERIC, controlType: 'numeric', Validators: null, IsEditable: true, IsFilterable: true, DefaultValue: null, viewCellStyle: "", IsVisible: false },
 		{ DisplayName: "EditPriceMinPCT", Name: "EditPriceMinPCT", DataType: DataTypes.NUMERIC, controlType: 'numeric', Validators: null, IsEditable: true, IsFilterable: true, DefaultValue: null, viewCellStyle: "", IsVisible: false }
 	]
-	@ViewChild('Header') BIGrid!: IGrid;
-	form: any = {};
-	formGroup!: FormGroup;
-	CategoryId: any;
-	dataItem: any;
-	constructor(public dataService: DataService, private fb: FormBuilder, private cd: ChangeDetectorRef) {
+	constructor(public dataService: DataService) {
 		this.dataService.APIURL = "https://bifmcg.bi-technologies.net/SalesBuzzOData/HH_AR_CustomerCategories";
 	}
-
-	// changeValue(event: any, controlName: string) {
-	// 	this.dataItem[controlName] = event.target.value
-	// }
-
-	// getDataItem(event: any) {
-	// 	this.dataItem = event.dataItem
-	// }
 
 	ngOnInit(): void {
 	}
 
-	ngAfterViewInit(): void {
-		if (this.BIGrid) {
-			this.columns.forEach(res => this.form[res.Name] = [{ value: res.DefaultValue, disabled: !res.IsEditable }, res.Validators]);
-			this.BIGrid.CurrentSelectRow = this.fb.group(this.form);
-			// this.BIGrid.CurrentSelectRow.valueChanges.subscribe(res => {
-			// 	if (this.dataItem && res) {
-			// 		this.dataItem['CategoryId'] = res['CategoryId'];
-			// 		this.dataItem['Description'] = res['Description'];
-			// 		this.dataItem['ArabicDescription'] = res['ArabicDescription'];
-			// 		this.dataItem['SalesTypeID'] = res['SalesTypeID'];
-			// 	}
-			// })
-			this.cd.detectChanges()
-		}
-	}
 }
